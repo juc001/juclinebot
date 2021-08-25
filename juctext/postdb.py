fad65f4bd1b8aed4dca8ef46508811e720db9834
cscs020 <rusa020@easybuy3c.com>
2021-08-26 05:20:07 +0800

    ok

--- juctext/postdb.py
+++ juctext/postdb.py
@@ -1,63 +1,51 @@
-import psycopg2
+import psycopg2,os
 from linebot.models import *
 
 #紀錄主程式
 line_bot_api = '1oBwTrLvaEqSse8SULJUFmSzGSrV2NWxr71/pgCjKqUs+jaNZ2iktP+8sU0ZxSNNr/CZrgT8GUcetwRo+PeUaG0L90LBTgrr43UesgCI6IjmyuusaVtbuhrmsoA8G/nndtQ49fw28V4opCDuL/Df4QdB04t89/1O/w1cDnyilFU='
 def insert_record(event):
-    
     if '學生紀錄' in event.message.text:
-        
+        remessages=TextSendMessage(text='try失敗了')
         try:
             record_list = prepare_record(event.message.text)
             reply = line_insert_record(record_list)
 
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text=reply)
+                remessages=TextSendMessage(text=reply)
             )
 
         except:
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text='失敗了')
+                remessages=TextSendMessage(text='匯入失敗了')
             )
-
-        return True
-    else:
-        return False
+    return remessages
 #準備資料
 def prepare_record(text):
     text_list = text.split('\n')
     record_list = []
     for i in text_list[1:]:
-        temp_list = i.split(',')
+        temp_list = i.split(';')
         temp_id = temp_list[0]
         temp_name = temp_list[1]
         temp_gender = temp_list[2]
         temp_grade = temp_list[3]
-        record = (temp_id, temp_name, temp_gender, temp_grade)
+        record = (temp_id,temp_name,temp_gender,temp_grade)
         record_list.append(record)
-    TextSendMessage(text='準備資料')
     return record_list
 #紀錄過程
 def line_insert_record(record_list):
-    DATABASE_URL = 'postgres://npiqezewrkddpe:0a97822788672e9ee3a0521b75427bf4d0b92f20e5fda20b44ebe3bf1128bc9d@ec2-18-214-238-28.compute-1.amazonaws.com:5432/d4u1bgq5fsb3a'
+    DATABASE_URL = os.popen('heroku config:get DATABASE_URL -a juctest').read()[:-1]
 
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
-
-    table_columns = '(id, name, gender, grade)'
-    postgres_insert_query = f"""INSERT INTO sudent {table_columns} VALUES (%s,%s,%s,%s)"""
-
-    cursor.executemany(postgres_insert_query, record_list)
+    cursor.executemany('INSERT INTO sudent(id, name, gender, grade) VALUES (%s,%s,%s,%s)', record_list)
     conn.commit()
-    TextSendMessage(text='過程')
-    message = f"恭喜您！ {cursor.rowcount} 筆資料成功匯入 sudent 表單！"
+    message =("恭喜您！"+ {cursor.rowcount}+" 筆資料成功匯入 sudent 表單！")
     print(message)
-
     cursor.close()
     conn.close()
-    
     return message
 
 
--- juctext/searchdb.py
+++ juctext/searchdb.py
@@ -2,21 +2,13 @@ import psycopg2
 import os
 
 def line_select_overall():
-    DATABASE_URL = 'postgres://npiqezewrkddpe:0a97822788672e9ee3a0521b75427bf4d0b92f20e5fda20b44ebe3bf1128bc9d@ec2-18-214-238-28.compute-1.amazonaws.com:5432/d4u1bgq5fsb3a'
-
+    DATABASE_URL = os.popen('heroku config:get DATABASE_URL -a juctest').read()[:-1]
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
-    
-    postgres_select_query = f"""SELECT * FROM student;"""
-    
-    cursor.execute(postgres_select_query)
-    raw = cursor.fetchmany(2)
+    cursor.execute('SELECT * FROM student;')
     message = []
-    
-    for i in raw:
-        message.append((i[0], i[1], i[2], i[3]))
-    
+    for i in 2:
+        message.append(i[0], i[1], i[2], i[3])
     cursor.close()
     conn.close()
-    
     return message
